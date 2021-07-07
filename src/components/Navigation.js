945628969a125429f82a44ca1f86773153b0c52f
Fabrice Berthet <berthet-fabrice@hotmail.fr>
2021-07-07 16:36:57 +0200

    ajout style

---
+++ src/components/Navigation.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navigation = () => {
+    return (
+        <div className="sidebar">
+            <div className="id">
+                <div className="idecontent">
+                    <img src="" alt="photo de profil" />
+                    <h3>Fabrice BERTHET</h3>
+                </div>
+            </div>
+
+            <div className="navigation">
+                <ul>
+                    <li>
+                        <NavLink exact to="/" activeClassName="navActive">
+                            <i className="fas fa-home"></i>
+                            <span>Accueil</span>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink exact to="/competences" activeClassName="navActive">
+                            <i className="fas fa-mountain"></i>
+                            <span>Compétences</span>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink exact to="/projets" activeClassName="navActive">
+                            <i className="fas fa-images"></i>
+                            <span>Projets</span>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink exact to="/contact" activeClassName="navActive">
+                            <i className="fas fa-address-book"></i>
+                            <span>Contact</span>
+                        </NavLink>
+                    </li>
+                </ul>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Navigation;
